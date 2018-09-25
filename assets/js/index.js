30e6b55ef2600ce1ea157758c91e75f71df3b51f
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-09-25 22:59:39 +0200

    update

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+//ScrollReveal - efekt wjeżdzających napisów
+    window.sr = ScrollReveal();
+      sr.reveal('#about',{ 
+       origin: 'left', 
+       distance: '50vw', 
+       duration: 2000, 
+       viewFactor: 0.5
+    });
+       
+      
